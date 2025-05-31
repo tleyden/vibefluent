1d786c0d644115c7049b07deb886e053c125d82a
Traun Leyden <traun.leyden@gmail.com>
2025-05-31 16:15:29 +0200

    add mode structs

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from enum import Enum
 
 
 class VocabWord(BaseModel):
@@ -9,8 +10,12 @@ class VocabWord(BaseModel):
     def __str__(self):
         return f"{self.word_in_target_language} ({self.word_in_native_language})"
 
+class Mode(Enum):
+    CONVERSATION_MODE = "conversation_mode"
+    DRILL_MODE = "drill_mode"
 
 class ConversationResponse(BaseModel):
     assistant_message: str
     follow_up_question: str
     vocab_words_user_asked_about: List[VocabWord] = []
+    user_requested_mode_change: Optional[Mode] = None
