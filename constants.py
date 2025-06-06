81a86eab9500277808e38b3d6757af07be418e71
Traun Leyden <traun.leyden@gmail.com>
2025-06-06 16:23:56 +0200

    add comments

--- constants.py
+++ constants.py
@@ -4,16 +4,16 @@ DEFAULT_REASONING_MODEL = "o1"
 DUMP_RAW_LLM_OUTPUT = False
 
 
+# Much cheaper, but doesn't seem to be able to handle function calls very well
 # DEFAULT_REALTIME_AUDIO_MODEL = "gpt-4o-mini-realtime-preview-2024-12-17"
+
 DEFAULT_REALTIME_AUDIO_MODEL = "gpt-4o-realtime-preview-2024-12-17"
 
 DEFAULT_REALTIME_AUDIO_VOICE = "shimmer"
 
 # Mode constants
 REALTIME_AUDIO_CONVERSATION = "REALTIME_AUDIO_CONVERSATION"
-TEXT = "TEXT"
+TEXT = "TEXT"  # This mode is for text-based conversations, but is less cool and not maintained
 
-# MODE = "TEXT"  # Options: "TEXT" or REALTIME_AUDIO_CONVERSATION
-
-# To enable realtime audio mode, change MODE to:
+# Set the mode 
 MODE = REALTIME_AUDIO_CONVERSATION
