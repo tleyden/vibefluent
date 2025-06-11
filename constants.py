05c9544763b3f61675cccc8f39c341df3c3a972e
Traun Leyden <traun.leyden@gmail.com>
2025-06-11 09:44:00 +0200

    update to latest gpt-4o-realtime-preview

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 DEFAULT_MODEL = "gpt-4o"
-DEFAULT_FALLBACK_MODEL = "claude-3-7-sonnet-latest" 
+DEFAULT_FALLBACK_MODEL = "claude-3-7-sonnet-latest"
 DEFAULT_REASONING_MODEL = "o1"
 DUMP_RAW_LLM_OUTPUT = False
 
@@ -7,7 +7,7 @@ DUMP_RAW_LLM_OUTPUT = False
 # Much cheaper, but doesn't seem to be able to handle function calls very well
 # DEFAULT_REALTIME_AUDIO_MODEL = "gpt-4o-mini-realtime-preview-2024-12-17"
 
-DEFAULT_REALTIME_AUDIO_MODEL = "gpt-4o-realtime-preview-2024-12-17"
+DEFAULT_REALTIME_AUDIO_MODEL = "gpt-4o-realtime-preview"
 
 DEFAULT_REALTIME_AUDIO_VOICE = "shimmer"
 
@@ -15,5 +15,5 @@ DEFAULT_REALTIME_AUDIO_VOICE = "shimmer"
 REALTIME_AUDIO_CONVERSATION = "REALTIME_AUDIO_CONVERSATION"
 TEXT = "TEXT"  # This mode is for text-based conversations, but is less cool and not maintained
 
-# Set the mode 
+# Set the mode
 MODE = REALTIME_AUDIO_CONVERSATION
